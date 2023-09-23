f9373cae3535e905b23fa46a97401bee996ee035
Yakkov david <yakkovdavid77@gmail.com>
2023-09-24 00:22:29 +0300

    traning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from 'react';
+import { Container, TextField, Button, List, ListItem, ListItemText, IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 
 function App() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTask = () => {
+    if (task !== '') {
+      setTasks([...tasks, task]);
+      setTask('');
+    }
+  };
+
+  const handleDeleteTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>Task Management App</h1>
+      <div>
+        <TextField 
+          variant="outlined" 
+          label="New Task" 
+          value={task} 
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <Button variant="contained" color="primary" onClick={handleAddTask}>
+          Add Task
+        </Button>
+      </div>
+      <List>
+        {tasks.map((t, index) => (
+          <ListItem key={index}>
+            <ListItemText primary={t} />
+            <IconButton edge="end" aria-label="delete" onClick={() => handleDeleteTask(index)}>
+              <DeleteIcon />
+            </IconButton>
+          </ListItem>
+        ))}
+      </List>
+    </Container>
   );
 }
 
 export default App;
+
+
