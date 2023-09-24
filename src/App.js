37871dfce294a0e5e877adaa8df780dc8e0c24fa
Yakkov david <yakkovdavid77@gmail.com>
2023-09-24 11:06:05 +0300

    updating

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, TextField, Button, List, ListItem, ListItemText, IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
-
 
 function App() {
   const [task, setTask] = useState('');
   const [tasks, setTasks] = useState([]);
 
+  // Load tasks from localStorage when the component mounts
+  useEffect(() => {
+    const savedTasks = localStorage.getItem('tasks');
+    if (savedTasks) {
+      setTasks(JSON.parse(savedTasks));
+    }
+  }, []);
+
   const handleAddTask = () => {
     if (task !== '') {
-      setTasks([...tasks, task]);
+      const newTasks = [...tasks, task];
+      setTasks(newTasks);
       setTask('');
+
+      // Save to localStorage
+      localStorage.setItem('tasks', JSON.stringify(newTasks));
     }
   };
 
@@ -19,6 +29,9 @@ function App() {
     const newTasks = [...tasks];
     newTasks.splice(index, 1);
     setTasks(newTasks);
+
+    // Save to localStorage
+    localStorage.setItem('tasks', JSON.stringify(newTasks));
   };
 
   return (
@@ -30,6 +43,12 @@ function App() {
           label="New Task" 
           value={task} 
           onChange={(e) => setTask(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              e.preventDefault();
+              handleAddTask();
+            }
+          }}
         />
         <Button variant="contained" color="primary" onClick={handleAddTask}>
           Add Task
@@ -50,5 +69,3 @@ function App() {
 }
 
 export default App;
-
-
